54e72ba85a47192b3093d5d87c6cfae85f558bfe
frumpymoons <pngmn@mailbox.org>
2019-09-09 02:49:06 +0200

    Remove unused stuff

--- Elements/ClassModules.lua
+++ Elements/ClassModules.lua
@@ -11,37 +11,6 @@ function ns.classModule.additionalPowerBar(self, config, uconfig)
 	self:Tag(self.AdditionalPower.Value, '[abu:additionalpower]')
 end
 
-function ns.classModule.MAGE(self, config, uconfig)
-	if (config.MAGE.showArcaneStacks) then
-		MageArcaneChargesFrame:SetParent(self)
-		MageArcaneChargesFrame:ClearAllPoints()
-		MageArcaneChargesFrame:SetPoint('TOP', self, 'BOTTOM', 30, -0.5)
-		--ns.PaintFrames(select(2, MonkHarmonyBar:GetRegions()), 0.1)
-		return MageArcaneChargesFrame
-	end
-end
-
-function ns.classModule.PALADIN(self, config, uconfig)
-	if (config.PALADIN.showHolyPower) then
-		PaladinPowerBarFrame:SetParent(self)
-		PaladinPowerBarFrame:SetScale(uconfig.scale * 0.81)
-		PaladinPowerBarFrame:ClearAllPoints()
-		PaladinPowerBarFrame:SetPoint('TOP', self, 'BOTTOM', 27, 4)
-		PaladinPowerBarFrame:SetFrameStrata("LOW");
-		ns.PaintFrames(PaladinPowerBarFrameBG, 0.1)
-		return PaladinPowerBarFrame
-	end
-end
-
-function ns.classModule.PRIEST(self, config, uconfig)
-	if (config.PRIEST.showInsanity) then
-		InsanityBarFrame:SetParent(self) 
-		InsanityBarFrame:ClearAllPoints()
-		InsanityBarFrame:SetPoint('BOTTOMRIGHT', self, 'TOPLEFT', 52, -50)
-		return InsanityBarFrame
-	end
-end
-
 function ns.classModule.ROGUE(self, config, uconfig)
 	if (config.ROGUE.showComboPoints) then
 		local ComboPoints = CreateFrame("Frame", self:GetName()..'ComboPointsBar', self)
@@ -73,25 +42,6 @@ function ns.classModule.ROGUE(self, config, uconfig)
 		end
 		self.ComboPointsBar = ComboPoints
 		return self.ComboPointsBar
-	end
-end
-
-function ns.classModule.WARLOCK(self, config, uconfig)
-	if (config.WARLOCK.showShards) then
-		WarlockPowerFrame:SetParent(self)
-		WarlockPowerFrame:ClearAllPoints()
-		WarlockPowerFrame:SetPoint('TOP', self, 'BOTTOM', 29, -2)
-		if (ns.config.playerStyle == 'normal') then 
-			WarlockPowerFrame:SetFrameStrata("LOW");
-		end
-		local shard = WarlockPowerFrame.Shards[#WarlockPowerFrame.Shards]
-		while shard do
-			--ns.PaintFrames(select(5,shard:GetRegions()), .2)
-
-			shard = shard.previousShard
-		end
-		ns.PaintFrames(WarlockPowerFrame:GetRegions(), 0)
-		return WarlockPowerFrame
 	end
 end
 
